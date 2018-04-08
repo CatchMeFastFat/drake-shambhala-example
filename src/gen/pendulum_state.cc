ebfcf60169f729138631733501a7de071096c4a7
CatchMeFastFat <bow.loaf@yahoo.com>
2018-04-08 23:03:13 +0200

    init

---
+++ src/gen/pendulum_state.cc
@@ -0,0 +1,25 @@
+#include "gen/pendulum_state.h"
+
+// GENERATED FILE DO NOT EDIT
+// See drake/tools/lcm_vector_gen.py.
+
+namespace drake {
+namespace examples {
+namespace pendulum {
+
+const int PendulumStateIndices::kNumCoordinates;
+const int PendulumStateIndices::kTheta;
+const int PendulumStateIndices::kThetadot;
+
+const std::vector<std::string>& PendulumStateIndices::GetCoordinateNames() {
+  static const never_destroyed<std::vector<std::string>> coordinates(
+      std::vector<std::string>{
+          "theta",     // BR
+          "thetadot",  // BR
+      });
+  return coordinates.access();
+}
+
+}  // namespace pendulum
+}  // namespace examples
+}  // namespace drake
